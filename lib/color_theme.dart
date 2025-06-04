974c81cc0bbd788bff2edb1a9f02077d59ca059f
gegerjohnpaul.gabayeron <gegerjohnpaul.gabayeron@students.isatu.edu.ph>
2025-06-04 07:04:47 +0000

    init color theme

---
+++ lib/color_theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const Color cardDefaultBackground = Color(0xffF5F5F5);
+  static const Color cardDefaultAccent = Color(0xffD9D9D9);
+  // static const Color cardDefaultBorder = Colors.transparent;
+
+  static const Color cardSelectedBackground = Color(0xffDAEDFF);
+  static const Color cardSelectedAccent = Color(0xff8CC7FF);
+  // static const Color cardSelectedBorder = Color(0xff0084FF);
+
+  static const Color cardText = Color(0xff000000);
+
+  static const Color buttonDefaultBackground = Color(0xff0084FF);
+  static const Color buttonAlternativeBackground = Color(0xffAFAFAF);
+}
